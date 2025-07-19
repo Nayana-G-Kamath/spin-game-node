9f04e0af77e5192f2f0b515f13b986314cfd1b70
Nayana-G-Kamath <nayanagkamath84@gmail.com>
2025-07-19 11:12:23 +0530

    second commit : latest changes

--- script.js
+++ script.js
@@ -8,6 +8,59 @@
 
 
 
+
+
+//7. restart the game again
+
+
+
+//promt variable creation
+const prompt = require("prompt-sync")();
+//1. collect some amount of money to deposite from the user
+const deposite = () => {
+  while (true) {
+    const depositeAmount = prompt("enter deposite amount: ");
+    const numberDepositeAmount = parseFloat(depositeAmount); //this will convert string to float & if user enters letters then sets NaN
+    if (isNaN(numberDepositeAmount) || numberDepositeAmount <= 0) {
+      console.log("Invalid Amount please try again");
+    } else {
+      return numberDepositeAmount;
+    }
+  }
+};
+
+const balance = deposite();
+//2. collect the number of lines to bid on
+const getLines = () => {
+  while (true) {
+    const lines = prompt("enter number of lines to bid on: ");
+    const numberOfLines = parseInt(lines); //this will convert string to float & if user enters letters then sets NaN
+    if (isNaN(numberOfLines) || numberOfLines > 3 || numberOfLines <=0) {
+      console.log("Invalid number of lines please try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+
+
+
+//3. collect betting amout
+const getBid = (balance,totalLines) => {
+  while (true) {
+    const biddingAmount = prompt("enter bidding amount: ");
+    const numberbiddingAmount = parseInt(biddingAmount); //this will convert string to float & if user enters letters then sets NaN
+    if (isNaN(biddingAmount) || numberbiddingAmount <=0 || numberbiddingAmount*totalLines>balance) {
+      console.log("Invalid amount please try again");
+    } else {
+      return numberbiddingAmount;
+    }
+  }
+};
+
+
+
 //4. spin the slot machine
 
   //define 3*3 matrix
@@ -77,60 +130,63 @@ for(let i=0; i<Num_ROWS; i++)
 return reels;
 }
 
+//printing the reels
+const printReels=(reels)=>{
+  for(let i=0; i<Num_ROWS; i++)
+  { 
+    let rowString="";
+    for(let j=0; j<NUM_COLS; j++)
+    {
+      rowString=rowString+reels[i][j];
+      if(j!=Num_ROWS-1)
+      {
+        rowString +=" | "
+      }
+    }
+    console.log(rowString);
+  }
+  
+}
 
 
-console.log(spin(SPIN_COUNT))
+
+// console.log(spin(SPIN_COUNT))
+
+
 
 
 //5. check if the user has won
-//6. give user their winnings
-//7. restart the game again
+//6. give user their winnings<both in one step>
+const winnings=(totalLines,reels,bid)=>
+{
+   let winnings=0;
+   for(let row=0; row<totalLines; row++)
+   {
+       const symbols=reels[row];
+       let allCorrect=true;
+       for(let symbol of symbols )
+       {
+        if(symbol!=symbols[0])
+        {
+          allCorrect=false;
+          break
+        }
+       }
+
+       if(allCorrect)
+       {
+        winnings=winnings+bid*SPIN_REWARDS[symbols[0]];
+       }
+   }
+
+   return winnings;
+}
 
 
-
-//promt variable creation
-const prompt = require("prompt-sync")();
-//1. collect some amount of money to deposite from the user
-const deposite = () => {
-  while (true) {
-    const depositeAmount = prompt("enter deposite amount: ");
-    const numberDepositeAmount = parseFloat(depositeAmount); //this will convert string to float & if user enters letters then sets NaN
-    if (isNaN(numberDepositeAmount) || numberDepositeAmount <= 0) {
-      console.log("Invalid Amount please try again");
-    } else {
-      return numberDepositeAmount;
-    }
-  }
-};
-
-const balance = deposite();
-//2. collect the number of lines to bid on
-const getLines = () => {
-  while (true) {
-    const lines = prompt("enter number of lines to bid on: ");
-    const numberOfLines = parseInt(lines); //this will convert string to float & if user enters letters then sets NaN
-    if (isNaN(numberOfLines) || numberOfLines > 3 || numberOfLines <=0) {
-      console.log("Invalid number of lines please try again");
-    } else {
-      return numberOfLines;
-    }
-  }
-};
-
+//function calls
 const totalLines=getLines();
-
-
-//3. collect betting amout
-const getBid = (balance,totalLines) => {
-  while (true) {
-    const biddingAmount = prompt("enter bidding amount: ");
-    const numberbiddingAmount = parseInt(biddingAmount); //this will convert string to float & if user enters letters then sets NaN
-    if (isNaN(biddingAmount) || numberbiddingAmount <=0 || numberbiddingAmount*totalLines>balance) {
-      console.log("Invalid amount please try again");
-    } else {
-      return numberbiddingAmount;
-    }
-  }
-};
-
-getBid(balance,totalLines);
+bid=getBid(balance,totalLines);
+reels=spin(SPIN_COUNT);
+printReels(reels);
+winAmount=winnings(totalLines,reels,bid)
+console.log("you won: "+ winAmount.toString());
